307f67fffd6beccf21e8bc638f82368b7e85c5b1
Tiger Wang <tigerwang@outlook.com>
2020-12-18 18:10:06 -0500

    init

---
+++ src/main/java/com/tensorsmart/invesla/InveslaApplication.java
@@ -0,0 +1,13 @@
+package com.tensorsmart.invesla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InveslaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InveslaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tensorsmart/invesla/InveslaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tensorsmart.invesla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InveslaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

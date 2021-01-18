4346d99cd974a6af2ebefd16de5edde30a8124fb
Tiger Wang <tigerwang@outlook.com>
2021-01-17 23:17:06 -0500

    fixed API path issue

--- src/main/java/com/tensorsmart/invesla/controller/StockController.java
+++ src/main/java/com/tensorsmart/invesla/controller/StockController.java
@@ -8,11 +8,9 @@ import org.openapitools.api.StocksApi;
 import org.openapitools.model.Stock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/stocks")
 public class StockController implements StocksApi {
 
     @Autowired

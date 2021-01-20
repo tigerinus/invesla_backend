35125449342506d618096c8a751027c115dfc456
Tiger Wang <tigerwang@outlook.com>
2021-01-19 20:20:46 -0500

    updated HiControllerTests to mock MVC and load spring application context at the same time.

--- src/test/java/com/tensorsmart/invesla/HiControllerTests.java
+++ src/test/java/com/tensorsmart/invesla/HiControllerTests.java
@@ -2,12 +2,14 @@ package com.tensorsmart.invesla;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 public class HiControllerTests {
 
     @Autowired

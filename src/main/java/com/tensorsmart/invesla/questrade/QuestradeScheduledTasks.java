566ab5a3a74983e86bc3d24f680ded4ca605f54b
Tiger Wang <tigerwang@outlook.com>
2021-02-14 23:28:42 -0500

    added scheduled task to  update market info daily

---
+++ src/main/java/com/tensorsmart/invesla/questrade/QuestradeScheduledTasks.java
@@ -0,0 +1,22 @@
+package com.tensorsmart.invesla.questrade;
+
+import com.tensorsmart.invesla.questrade.service.MarketService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class QuestradeScheduledTasks {
+    
+    @Autowired
+    private MarketService _marketService;
+
+    @EventListener(ApplicationReadyEvent.class)
+    @Scheduled(zone = "EST", cron = "0 0 1 * * MON-FRI")
+    public void refreshMarkets() {
+        _marketService.updateMarkets();
+    }
+}
